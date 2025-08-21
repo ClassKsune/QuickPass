32a9e882b1366c08967a1e947a438ecdc6bfe3fc
Artie <felixmorningstar@proton.me>
2025-08-21 22:24:31 +0200

    Added translation and edited orders

--- app/api/order/orderFunctions.js
+++ app/api/order/orderFunctions.js
@@ -8,7 +8,7 @@ export const createOrder = (data) => Order(data)
 export const changeOrderStatus = (filter, status) => updateMongo(Order, filter, { status })
 
 export const getOrders = async (filter = {}) => {
-  const orders = await findAllMongo(Order, filter);
+  const orders = await findAllMongo(Order); // findAllMongo(Order, filter); if you want to filter only your own orders
   const ordersWithProfiles = []
   await Promise.all(
     orders.map(async (order) => {
